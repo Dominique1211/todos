0c22a096f858438c100eb62ae9deaefa7a4e2805
Dominique1211 <sunshine.p2525@gmail.com>
2022-10-08 01:11:41 +0000

    first

--- src/app.js
+++ src/app.js
@@ -5,7 +5,93 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+
+// console.log("Hello");
+
+// let todosItems = [];
+
+// function printTodos() {
+//   const ul = document.querySelector(".todos");
+//   let content = "";
+//   Todos.forEach(function(item) {
+//     console.log("Content", content);
+//     content = content + "<li>" + item + "</li>";
+//   });
+//   content = content + "<li></li>";
+//   // ul.innerHTML = content;
+// }
+// function addTodo(label) {
+//   Todos.push(label);
+//   printTodos();
+// }
+
+// window.addTodo = addTodo;
+// printTodos();
+
+let todoItems = [];
+
+function renderTodo(todo) {
+  const list = document.querySelector(".js-todo-list");
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+
+  const isChecked = todo.checked ? "done" : "";
+  const node = document.createElement("li");
+  node.setAttribute("class", `todo-item ${isChecked}`);
+  node.setAttribute("data-key", todo.id);
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  if (item) {
+    list.replaceChild(node, item);
+  } else {
+    list.append(node);
+  }
+}
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now()
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+
+function toggleDone(key) {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}
+
+const form = document.querySelector(".js-form");
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const input = document.querySelector(".js-todo-input");
+
+  const text = input.value.trim();
+  if (text !== "") {
+    addTodo(text);
+    input.value = "";
+    input.focus();
+  }
+});
+
+const list = document.querySelector(".js-todo-list");
+list.addEventListener("click", event => {
+  if (event.target.classList.contains("js-tick")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+});
